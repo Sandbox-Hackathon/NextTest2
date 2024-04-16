7aea2abec5e3cde668bd3231af16ad0129fb1ad4
SnailMailShale <10879157@uvu.edu>
2024-04-15 22:33:16 -0600

    Second Commit

---
+++ app/test.tsx
@@ -0,0 +1,9 @@
+"use client";
+export default function Test() {
+    return (
+      <>
+        <p>Hello World</p>
+        <img src="https://miro.medium.com/v2/resize:fit:1358/1*GI-td9gs8D5OKZd19mAOqA.png" onClick={() => alert("Oh no")}></img>
+      </>
+    );
+  }
